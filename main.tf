12ed819297b595f4f9dbab6201c4c074596d43b0
DHEERAJBHANURU <142112229+DHEERAJBHANURU@users.noreply.github.com>
2024-11-17 12:32:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 2
-ami = "ami-03eb6185d756497f8"
+ami = "ami-0984f4b9e98be44bf"
 instance_type = "t2.micro"
 subnet_id = "subnet-0674620a5c12a9c0c"
 
